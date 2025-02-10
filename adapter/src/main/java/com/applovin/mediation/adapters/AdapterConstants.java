7808c853f924251dd67fc4a340413c5c4e5c0dc5
omnibaker <37693234+omnibaker@users.noreply.github.com>
2025-02-10 16:05:34 +1100

    1.6.4-rc.0

--- adapter/src/main/java/com/applovin/mediation/adapters/AdapterConstants.java
+++ adapter/src/main/java/com/applovin/mediation/adapters/AdapterConstants.java
@@ -2,7 +2,7 @@ package com.applovin.mediation.adapters;
 
 public class AdapterConstants {
 
-    public static final String ADAPTER_VERSION = "1.8.1";
+    public static final String ADAPTER_VERSION = "1.8.3-rc.0";
     public static final String ADAPTER_TYPE = "APPLOVIN";
     public static final String PARAM_APP_ID = "app_id";
     public static final String PARAM_CPM_FLOOR = "cpm_floor";
